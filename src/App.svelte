accd1aec93eeb198d54f7828bca87d9390893685
Daniel Bulant <danbulant@gmail.com>
2021-11-10 14:25:33 +0100

    Page switch effect

--- src/App.svelte
+++ src/App.svelte
@@ -4,29 +4,41 @@
 	import UserDrawer from "./Components/UserDrawer.svelte";
 	import { user } from "./Stores/user";
 	import { users } from "./Stores/db";
-import Chooser from "./Components/Chooser.svelte";
-import ProjectList from "./Pages/ProjectList.svelte";
+	import Chooser from "./Components/Chooser.svelte";
+	import ProjectList from "./Pages/ProjectList.svelte";
+	import Carousel from "./Components/Carousel.svelte";
 
 	var openUser = true;
 	var page = "users";
+	var setCurrentPage;
+
+	$: {
+		if(setCurrentPage) setCurrentPage(["users", "projects"].indexOf(page));
+	}
 </script>
 
-<main class="select-none">
+<main class="select-none h-full flex flex-col" style="">
 	{#if !$user}
 		<Home />
 	{:else}
-		<div class="flex items-center px-4 mb-2 shadow">
+		<div class="flex-0 flex items-center px-4 mb-2 shadow">
 			<h1 class="text-2xl">Conwo</h1>
 			<div class="flex-grow"></div>
 			<small class="mr-1 cursor-pointer" on:click={() => openUser = false}>{$user} &middot;</small>
 			<small class="cursor-pointer" on:click={() => $user = null}>Log out</small>
 		</div>
 
-		{#if page === "users"}
-			<UserList />
-		{:else}
-			<ProjectList />
-		{/if}
+		<Carousel pageCount={2} showDots={false} bind:setCurrentPage>
+			<div class="h-full">
+				<UserList />
+			</div>
+			<div class="h-full">
+				<ProjectList />
+			</div>
+		</Carousel>
+	{/if}
+	{#if $user}
+		<div class="h-8"></div>
 	{/if}
 </main>
 	
@@ -40,6 +52,8 @@ import ProjectList from "./Pages/ProjectList.svelte";
 		font-family: 'Roboto', sans-serif;
 		margin: 0;
 		padding: 0;
+		height: 100vh;
+		width: 100vw;
 	}
 	:global(*) {
 		-webkit-tap-highlight-color: transparent;
---
+++ src/Components/Carousel.svelte
@@ -0,0 +1,103 @@
+<script>
+    import { tweened } from "svelte/motion";
+    import { expoOut } from "svelte/easing";
+    export let pageCount = 1;
+    export let showDots = true;
+    let scroller;
+    let scrollerWidth;
+    let contentWidth;
+    let scrollX = 0;
+    let mouseDown = false;
+    export let currentPage = 0;
+    export const onScroll = () => {
+        scrollX = scroller.scrollLeft;
+    };
+    export const setCurrentPage = (i) => {
+        applyNativeSnap = false;
+        targetScrollLeft = i * scrollerWidth;
+    };
+    $: contentWidth = scrollerWidth * pageCount;
+    $: scrollNormalized = scrollX / contentWidth;
+    $: currentPage = Math.round(scrollNormalized / (1 / pageCount));
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+    let applyNativeSnap = true;
+    let targetScrollLeft = 0;
+    const mouseDownHandler = (e) => {
+        applyNativeSnap = false;
+        mouseDown = true;
+        pos = {
+            // The current scroll
+            left: scroller.scrollLeft,
+            top: scroller.scrollTop,
+            // Get the current mouse position
+            x: e.clientX,
+            y: e.clientY,
+        };
+        document.addEventListener("mousemove", mouseMoveHandler);
+        document.addEventListener("mouseup", mouseUpHandler);
+    };
+    const mouseMoveHandler = (e) => {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+        targetScrollLeft = pos.left - dx;
+    };
+    const mouseUpHandler = (e) => {
+        document.removeEventListener("mousemove", mouseMoveHandler);
+        document.removeEventListener("mouseup", mouseUpHandler);
+        applyNativeSnap = false;
+        mouseDown = false;
+        targetScrollLeft = currentPage * scrollerWidth;
+    };
+    const tweenedScrollLeft = tweened(0, { duration: 200, easing: expoOut });
+    $: tweenedScrollLeft.set(targetScrollLeft);
+    $: if (scroller) scroller.scrollLeft = $tweenedScrollLeft;
+    $: if (targetScrollLeft === $tweenedScrollLeft && !mouseDown) {
+        applyNativeSnap = true;
+    }
+</script>
+
+<div class="flex flex-col w-full h-full overflow-visible">
+    <div
+        class="flex flex-grow w-full overflow-hidden --hide-scrollbar --scroller"
+        class:--smooth-snap={applyNativeSnap}
+        bind:this={scroller}
+        bind:clientWidth={scrollerWidth}
+        on:scroll={onScroll}
+        on:mousedown={mouseDownHandler}
+    >
+        <slot />
+    </div>
+
+    {#if showDots}
+        <div
+            class="flex items-center justify-center h-4 mt-2 space-x-2 transition-opacity duration-300 transform -translate-y-4"
+            class:opacity-0={pageCount < 2}
+        >
+            {#each Array(pageCount) as _, i}
+                <button
+                    class="w-2 h-2 transition-all duration-200 bg-gray-400 rounded-full outline-none focus:outline-none"
+                    class:--current-page-dot={currentPage === i}
+                    on:click={() => setCurrentPage(i)}
+                />
+            {/each}
+        </div>
+    {/if}
+</div>
+
+<style>
+    .--smooth-snap {
+        scroll-behavior: smooth;
+        scroll-snap-type: x mandatory;
+        overscroll-behavior-x: contain;
+    }
+    .--scroller > :global(*) {
+        flex-basis: 100%;
+        flex-shrink: 0;
+        flex-grow: 0;
+        scroll-snap-align: center;
+    }
+    .--current-page-dot {
+        background-color: #ff2d55;
+        transform: scale(1.5);
+    }
+</style>
